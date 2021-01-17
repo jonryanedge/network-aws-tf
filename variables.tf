5a58c7d71beb7a49af532adaaaf3c9d3207e4665
Jon Edgeworth <edgeworthjr@gmail.com>
2021-01-17 10:59:51 -0500

    Updated tf build and variables for us-west-1 deployment

--- variables.tf
+++ variables.tf
@@ -1,64 +1,64 @@
 variable "region" {
-  default = "us-east-1"
+  default = "us-west-1"
   description = "Region to deploy (default is us-east-1)"
 }
 
 variable "regionAsn" {
-  default = "65500"
+  default = "65530"
   description = "ASN for Regional Core Router (TGW)"
 }
 
 variable "regionCidr" {
-  default = "10.0.0.0/16"
+  default = "10.3.0.0/16"
   description = "CIDR Assignment for Core Router (TGW)"
 }
 
 variable "coreCidr" {
-  default = "10.0.0.0/24"
+  default = "10.3.0.0/24"
   description = "CIDR Assignment for Core VPC"
 }
 
 variable "edgeCidr" {
-  default = "10.0.1.0/24"
+  default = "10.3.1.0/24"
   description = "CIDR Assignment for Edge VPC"
 }
 
 variable "coreSubnets" {
   type = map
   default = {
-    "core1" = "10.0.0.0/27"
-    "core2" = "10.0.0.32/27"
-    "core3" = "10.0.0.64/27"
-    "core4" = "10.0.0.96/27"
-    "shared1" = "10.0.0.128/27"
-    "shared2" = "10.0.0.160/27"
-    "shared3" = "10.0.0.192/27"
-    "shared4" = "10.0.0.224/27"
+    "core1" = "10.3.0.0/27"
+    "core2" = "10.3.0.32/27"
+    "core3" = "10.3.0.64/27"
+    "core4" = "10.3.0.96/27"
+    "shared1" = "10.3.0.128/27"
+    "shared2" = "10.3.0.160/27"
+    "shared3" = "10.3.0.192/27"
+    "shared4" = "10.3.0.224/27"
   }
 }
 
 variable "edgeSubnets" {
   type = map
   default = {
-    "private1" = "10.0.1.0/27"
-    "private2" = "10.0.1.32/27"
-    "private3" = "10.0.1.64/27"
-    "private4" = "10.0.1.96/27"
-    "public1" = "10.0.1.128/27"
-    "public2" = "10.0.1.160/27"
-    "public3" = "10.0.1.192/27"
-    "public4" = "10.0.1.224/27"
+    "private1" = "10.3.1.0/27"
+    "private2" = "10.3.1.32/27"
+    "private3" = "10.3.1.64/27"
+    "private4" = "10.3.1.96/27"
+    "public1" = "10.3.1.128/27"
+    "public2" = "10.3.1.160/27"
+    "public3" = "10.3.1.192/27"
+    "public4" = "10.3.1.224/27"
   }
 }
 
 variable "memberVpc" {
   type = map
   default = {
-    "vpcCidr" = "10.0.10.0/24"
-    "member1" = "10.0.10.0/27"
-    "member2" = "10.0.10.32/27"
-    "member3" = "10.0.10.64/27"
-    "member4" = "10.0.10.96/27"
+    "vpcCidr" = "10.3.10.0/24"
+    "member1" = "10.3.10.0/27"
+    "member2" = "10.3.10.32/27"
+    "member3" = "10.3.10.64/27"
+    "member4" = "10.3.10.96/27"
   }
 }
 
@@ -76,17 +76,17 @@ variable "dxgw" {
   type = map
   default = {
     "dxAsn" = "65505"
-    "dxCidr" = "10.0.0.0/16"
+    "dxCidr" = "10.3.0.0/16"
   }
 }
 
 variable "rt53resolvers" {
   type = map
   default = {
-    "resolverIp1" = "10.0.0.140"
-    "resolverIp2" = "10.0.0.170"
-    "forwarderIp1" = "10.0.0.141"
-    "forwarderIp2" = "10.0.0.171"
+    "resolverIp1" = "10.3.0.140"
+    "resolverIp2" = "10.3.0.170"
+    "forwarderIp1" = "10.3.0.141"
+    "forwarderIp2" = "10.3.0.171"
     "targetIp1" = "8.8.8.8"
     "targetIp2" = "8.8.4.4"
   }
