e76ca88ca20e197050d6c925e38c196c3528ed13
Jon Edgeworth <edgeworthjr@gmail.com>
2021-01-25 07:03:14 -0500

    Added edge vpc security group

--- securityGroups.tf
+++ securityGroups.tf
@@ -1,6 +1,30 @@
-resource "aws_security_group" "sg1" {
-  name = "defaultAllow"
+resource "aws_security_group" "coreSG" {
+  name = "defaultCore"
   vpc_id = aws_vpc.core.id
+  
+  ingress {
+    description = "allowAll"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-all"
+  }
+}
+
+resource "aws_security_group" "edgeSG" {
+  name = "defaultEdge"
+  vpc_id = aws_vpc.edge.id
   
   ingress {
     description = "allowAll"
