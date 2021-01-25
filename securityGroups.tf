b73013f99709f771a01d18e9c482196667e70990
Jon Edgeworth <edgeworthjr@gmail.com>
2021-01-24 22:06:58 -0500

    Added sg1 rule

--- securityGroups.tf
+++ securityGroups.tf
@@ -1,5 +1,23 @@
 resource "aws_security_group" "sg1" {
-  name = "security group 1"
+  name = "sg-allow-all"
   vpc_id = aws_vpc.core.id
   
+  ingress {
+    description = "allowAll"
+    from_port   = 0
+    to_port     = 40
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-all"
+  }
 }
